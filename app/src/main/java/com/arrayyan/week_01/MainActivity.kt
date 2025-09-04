d74ed57d9cd59f9ce7c91385140f5768dd298d1d
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2025-09-04 07:47:18 +0700

    Initial commit

---
+++ app/src/main/java/com/arrayyan/week_01/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.arrayyan.week_01
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.Gravity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val nameDisplay = findViewById<TextView>(R.id.name_display)
+        val studentDisplay = findViewById<TextView>(R.id.student_display)
+        val nameSubmit = findViewById<Button>(R.id.name_submit)
+
+        nameSubmit.setOnClickListener {
+            val nameInput = findViewById<TextInputEditText>(R.id.name_input)?.text.toString().trim()
+            val studentInput = findViewById<TextInputEditText>(R.id.student_input)?.text.toString().trim()
+
+            if (nameInput.isNotEmpty()) {
+                if (studentInput.length == 11) {
+                    nameDisplay.text = "${getString(R.string.name_greet)} $nameInput"
+                    studentDisplay.text = "Student Number: $studentInput"
+                } else {
+                    Toast.makeText(this, "Student number has to be 11 digits long", Toast.LENGTH_LONG)
+                        .apply {
+                            setGravity(Gravity.CENTER, 0, 0)
+                            show()
+                        }
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.name_empty), Toast.LENGTH_LONG)
+                    .apply {
+                        setGravity(Gravity.CENTER, 0, 0)
+                        show()
+                    }
+            }
+        }
+
+        val nameInput = findViewById<TextInputEditText>(R.id.name_input)?.text.toString().trim()
+        val studentInput = findViewById<TextInputEditText>(R.id.student_input)?.text.toString().trim()
+
+        if (nameInput.isNotEmpty()) {
+            if (studentInput.length == 11) {
+                nameDisplay.text = "${getString(R.string.name_greet)}\n$nameInput\n$studentInput"
+            } else {
+                Toast.makeText(this, "Student number has to be 11 digits long", Toast.LENGTH_LONG)
+                    .apply {
+                        setGravity(Gravity.CENTER, 0, 0)
+                        show()
+                    }
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.name_empty), Toast.LENGTH_LONG)
+                .apply {
+                    setGravity(Gravity.CENTER, 0, 0)
+                    show()
+                }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
